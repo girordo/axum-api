e812961a31b1506fe6ba29f8e4e39b64b82b888f
girordo <tsgiroldo@gmail.com>
2023-06-16 19:26:07 -0300

    feat(Cargo.toml, main.rs): new features add

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 #![allow(unused)]
 
-use axum::response::Html;
+use axum::extract::Query;
+use axum::response::{Html, IntoResponse};
 use axum::routing::{get, Router};
+use serde::Deserialize;
 use std::net::SocketAddr;
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(|| async { Html("Hello <strong>World!!!!</strong>") }),
-    );
+    let routes_hello = Router::new().route("/hello", get(handler_hello));
     println!("Hello, world!");
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
@@ -18,4 +22,12 @@ async fn main() {
         .serve(routes_hello.into_make_service())
         .await
         .unwrap();
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("World!!");
+
+    Html("Hello <strong>{name}</strong>")
 }
